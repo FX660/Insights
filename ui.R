1142e7591babff280c829a314e94473be22f85c1
Simon Otziger <simon@volemont.com>
2015-03-21 10:47:35 +0800

    Fix #1

--- ui.R
+++ ui.R
@@ -1,54 +1,54 @@
 
 shinyUI(
-  navbarPage("Insights", id="nav",      
-             
+  navbarPage("Insights", id="nav",
+
              tabPanel("Trade Charts",
-                      div(class="outer",      
+                      div(class="outer",
                           # Controls
-                          absolutePanel(id="controls", class="modal", fixed=TRUE, draggable=TRUE,
-                                        top=60, left="auto", right=20, bottom="auto", width=330, 
+                          absolutePanel(id="controls", class="panel panel-default", fixed=TRUE, draggable=TRUE,
+                                        top=60, left="auto", right=20, bottom="auto", width=330,
                                         height="auto",
-                                        
+
                                         h2("Controls"),
-                                        
+
                                         # account, portfolio, symbol
                                         fluidRow(
                                           column(width=4,
-                                                 selectInput("account", label="Account", 
+                                                 selectInput("account", label="Account",
                                                              choices=c("Choose an account"="", accounts))
                                           ),
                                           column(width=4,
                                                  conditionalPanel("input.account",
-                                                                  selectInput("portfolio", label="Portfolio", 
+                                                                  selectInput("portfolio", label="Portfolio",
                                                                               choices=c("Choose a portfolio"=""))
                                                  )
                                           ),
                                           column(width=4,
                                                  conditionalPanel("input.portfolio",
-                                                                  selectInput("symbol", label="Symbol(s)", 
-                                                                              choices=c("Choose a symbol"=""), 
+                                                                  selectInput("symbol", label="Symbol(s)",
+                                                                              choices=c("Choose a symbol"=""),
                                                                               multiple=TRUE)
                                                  )
                                           )
                                         ), #end fluidRow
                                         # date slider
-                                        sliderInput("dateRange", label="Date range", min=dateRange[1], 
+                                        sliderInput("dateRange", label="Date range", min=dateRange[1],
                                                     max=dateRange[2], value=dateRange, step=1)
                           ), #end absolutePanel
                           plotOutput("chartPosn", width="100%", height="100%")
                       ) #end div
              ), #end tabPanel
-             
+
              tabPanel("Trade Stats",
                       # account, portfolio, symbol
                       fluidRow(
                         column(width=2,
-                               selectInput("account_ts", label="Account", 
+                               selectInput("account_ts", label="Account",
                                            choices=c("Choose an account"="", accounts))
                         ),
                         column(width=2,
                                conditionalPanel("input.account_ts",
-                                                selectInput("portfolio_ts", label="Portfolio", 
+                                                selectInput("portfolio_ts", label="Portfolio",
                                                             choices=c("Choose a portfolio"=""))
                                )
                         )
@@ -70,7 +70,7 @@ shinyUI(
                                            choices=c("No"=FALSE, "Yes"=TRUE))
                         ),
                         column(width=2,
-                               numericInput("digits_ts", label="Digits to display", 
+                               numericInput("digits_ts", label="Digits to display",
                                             value=2, min=0, max=10)
                         ),
                         column(width=2,
@@ -130,7 +130,7 @@ shinyUI(
                           tags$div(),
 
                           # Controls
-                          absolutePanel(id="controls", class="modal", fixed=TRUE, draggable=TRUE,
+                          absolutePanel(id="controls", class="panel panel-default", fixed=TRUE, draggable=TRUE,
                                         top=60, left="auto", right=20, bottom="auto", width=330,
                                         height="auto",
 
@@ -169,7 +169,7 @@ shinyUI(
                                                                        "Percent"="percent",
                                                                        "Tick"="tick"))
                                           )
-                                        ) #end fluidRow                
+                                        ) #end fluidRow
                           ), #end absolutePanel
                           plotOutput("chartME", width="100%", height="100%")
                       ) #end div
@@ -183,7 +183,7 @@ shinyUI(
                           tags$div(),
 
                           # Controls
-                          absolutePanel(id="controls", class="modal", fixed=TRUE, draggable=TRUE,
+                          absolutePanel(id="controls", class="panel panel-default", fixed=TRUE, draggable=TRUE,
                                         top=60, left="auto", right=20, bottom="auto", width=330,
                                         height="auto",
 
@@ -220,7 +220,7 @@ shinyUI(
                           ), #end absolutePanel
                           plotOutput("chartEquityCurve", width="100%", height="100%")
                       ) #end div
-             ), #end tabPanel           
+             ), #end tabPanel
 
              conditionalPanel("false", icon("crosshair"))
   ) #end navbarPage
